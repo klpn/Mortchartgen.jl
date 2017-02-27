00a18f38ee5b352def2f230f74c017738597438f
Karl Pettersson <karl_pettersson_1998@yahoo.com>
2017-02-27 18:00:52 +0100

    Removed commenting out

--- src/Mortchartgen.jl
+++ src/Mortchartgen.jl
@@ -554,15 +554,15 @@ function writeplotsite(framedict, language,
 				remove_destination = true)
 		end
 	end
-	#for plottype in keys(conf["plottypes"])
-	#	batchplotdict = batchplot(framedict, language, plottype)
-	#	writeplotlist(batchplotdict, normpath(sitepath, "$plottype.html"))
-	#end
-	#chartdestpath = sitefullsubpaths["charts"]
-	#for chartfile in readdir(chartpath)
-	#	mv(normpath(chartpath, chartfile), normpath(chartdestpath, chartfile),
-	#	remove_destination = true)
-	#end
+	for plottype in keys(conf["plottypes"])
+		batchplotdict = batchplot(framedict, language, plottype)
+		writeplotlist(batchplotdict, normpath(sitepath, "$plottype.html"))
+	end
+	chartdestpath = sitefullsubpaths["charts"]
+	for chartfile in readdir(chartpath)
+		mv(normpath(chartpath, chartfile), normpath(chartdestpath, chartfile),
+		remove_destination = true)
+	end
 	for fname in ["default"; "index"; "site"; "mortchartdoc"]
 		writetempl(language, fname, sitepath)
 	end
